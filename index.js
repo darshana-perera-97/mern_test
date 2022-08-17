e7ac9ce83eb11b5215d0dc6ae26d769497cb7a91
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-17 20:50:23 +0530

    proxy created

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const port = 3001;
 
+//connect db
 
+//data schema
+
+//read
+
+//create
+
+//update
+
+//delete
 
 app.listen(port, function () {
   console.log("Express is running");
