85b7dfe5178444833566c5ce678039ff3b33f8e7
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-17 13:22:04 +0530

    code structure created

--- server.js
+++ server.js
@@ -4,4 +4,18 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const port = 3001;
 
+//connect mongodb
+
+//datra schemas
+
+//data models
+
+//read routes
+
+//create route
+
+//delete route
+
+//update route
+
 
